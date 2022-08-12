8e8505726a82d15a2a9932df1b4ddf75fa63a81f
Andrii Nepliakh <57762723+AndrewNepliakh@users.noreply.github.com>
2022-08-12 12:37:41 +0300

    Clear all scripts

--- Assets/Scripts/GameEnterPoint.cs
+++ Assets/Scripts/GameEnterPoint.cs
@@ -6,9 +6,8 @@ public class GameEnterPoint : MonoBehaviour
     {
         var saveData = SaveManager.Load();
         UserManager.Init(saveData.UserData);
-
-        Debug.Log(UserManager.CurrentUser.Character.Stats.NickName);
-        Debug.Log(UserManager.CurrentUser.ID);
+        
+        
     }
 
     private void OnApplicationQuit()
--- Assets/Scripts/Managers/UserManager/User.cs
+++ Assets/Scripts/Managers/UserManager/User.cs
@@ -4,45 +4,7 @@ using System.Linq;
 [Serializable]
 public class User
 {
-    private int _id;
-    private Character _character;
-    public int ID
-    {
-        get
-        {
-            if (_id > 0) return _id;
-            GetID();
-            return _id;
-        }
-        set
-        {
-            if (value > 0) _id = value;
-            else
-            {
-                GetID();
-            }
-        }
-    }
-
-    public Character Character
-    {
-        get => _character;
-        set { _character = value ?? new Player();}
-    }
-
-
     public User()
     {
-        GetID();
-        _character = new Player();
-    }
-    
-    private void GetID()
-    {
-        var id = 1;
-        var ids = UserManager.Users.Select(x => x._id).ToList();
-        var ascendingOrder = ids.OrderBy(x => x);
-        foreach (var x in ascendingOrder) if(x == id) id++;
-        _id = id;
     }
 }
