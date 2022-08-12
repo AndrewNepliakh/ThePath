f41b2c4a311849cc7b1be1f0f733bb3ac809b6ff
Andrii Nepliakh <57762723+AndrewNepliakh@users.noreply.github.com>
2022-08-12 12:42:19 +0300

    Setup GlobalMonoInstaller

---
+++ Assets/Scripts/GameEnterPoint/GlobalMonoInstaller.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using Zenject;
+
+public class GlobalMonoInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameEnterPoint/SOInstaller.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using Zenject;
+
+[CreateAssetMenu(fileName = "SOInstaller", menuName = "Installers/SOInstaller")]
+public class SOInstaller : ScriptableObjectInstaller<SOInstaller>
+{
+    public override void InstallBindings()
+    {
+        
+    }
+}
