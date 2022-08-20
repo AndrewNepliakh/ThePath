5d542a622f1186ccd9aa9fff51fd44be49496a0a
Andrii Nepliakh <57762723+AndrewNepliakh@users.noreply.github.com>
2022-08-20 19:40:43 +0300

    Implemented StateManager

---
+++ Assets/Scripts/Managers/StateManager/GameState.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Zenject;
+
+namespace Managers
+{
+    public class GameState : IState
+    {
+        public void Enter()
+        {
+        }
+
+        public void Exit()
+        {
+        }
+
+        public void Update()
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/StateManager/InitialState.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Zenject;
+
+namespace Managers
+{
+    public class InitialState : IState
+    {
+        public void Enter()
+        {
+        }
+
+        public void Exit()
+        {
+        }
+
+        public void Update()
+        {
+        }
+
+        private void LoadUserData()
+        {
+        }
+    }
+}
