e1c8d14bc6a2a01301a10275081fb518b13ce845
Andrii Nepliakh <57762723+AndrewNepliakh@users.noreply.github.com>
2022-10-31 21:06:40 +0200

    Implemented Inventory scene

---
+++ Assets/Scripts/Editor/SaveDataEditorToolMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class SaveDataEditorToolMenu
+{
+    private const string OpenSaveFileMenu = "Tools/Open Save File Directory";
+    private const string DeleteSaveFileMenu = "Tools/Delete Save File Directory";
+    
+    [MenuItem(OpenSaveFileMenu)]
+    private static void OpenSaveFileLocation()
+    {
+#if UNITY_EDITOR_WIN
+        if (Directory.Exists(Application.persistentDataPath))
+            System.Diagnostics.Process.Start(Application.persistentDataPath);
+#endif
+    }
+    
+    [MenuItem(DeleteSaveFileMenu)]
+    private static void DeleteSaveFileLocation()
+    {
+        if (Directory.Exists(Application.persistentDataPath))
+        {
+            foreach (var directory in Directory.GetDirectories(Application.persistentDataPath))
+            {
+                DirectoryInfo dir = new DirectoryInfo(directory);
+                dir.Delete(true);
+            }
+     
+            foreach (var file in Directory.GetFiles(Application.persistentDataPath))
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                fileInfo.Delete();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Editor/ScenesToolUtility.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+
+public class ScenesToolUtility
+{
+    [MenuItem("Scenes/InitScene")]
+    public static void InitScene() => OpenEditorScene("InitScene");
+
+    [MenuItem("Scenes/BattleScene")]
+    public static void BattleScene() => OpenEditorScene("BattleScene");
+
+    [MenuItem("Scenes/InventoryScene")]
+    public static void InventoryScene() => OpenEditorScene("InventoryScene");
+
+    [MenuItem("Scenes/MenuScene")]
+    public static void MenuScene() => OpenEditorScene("MenuScene");
+
+
+    static void OpenEditorScene(string sceneName)
+    {
+        if (Application.isPlaying)
+            return;
+
+        EditorSceneManager.OpenScene(Application.dataPath + "/Scenes/" + sceneName + ".unity");
+    }
+}
