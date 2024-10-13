01e0172833795c477552445330a06ba29fca980c
Andrii Nepliakh <andriinepliakh@gmail.com>
2024-10-13 20:29:02 +0300

    adressables adresses edits

--- Assets/Scripts/Managers/UIManager/Views/ChooseActionWindow/ChooseCoverButton.cs
+++ Assets/Scripts/Managers/UIManager/Views/ChooseActionWindow/ChooseCoverButton.cs
@@ -2,45 +2,45 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-
-public class ChooseCoverButton : MonoBehaviour
+namespace UI
 {
-    [SerializeField] private Button _button;
-    [SerializeField] private RectTransform _rectTransform;
-    public Action<Vector3> OnButtonClicked;
-    private Camera _camera;
-    private Cover _cover;
-
-    private void Awake()
+    public class ChooseCoverButton : MonoBehaviour
     {
-        _camera = Camera.main;
-    }
-
-    private void OnEnable()
-    {
-        _button.onClick.AddListener(OnButtonClickedAdd);
-    }
-
-    public void SetPosition(Cover cover, RectTransform parentRect)
-    {
-        _cover = cover;
-        var yOffset = _cover.transform.localScale.y;
-        var worldToScreenPoint = _camera.WorldToScreenPoint(cover.transform.position + Vector3.up * yOffset);
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(
-            parentRect, worldToScreenPoint, null, out var localPoint);
-        _rectTransform.anchoredPosition = localPoint;
-    }
-
-    private void OnButtonClickedAdd()
-    {
-        gameObject.SetActive(false);
-        OnButtonClicked?.Invoke(_cover.GetProperPosition());
-    }
+        [SerializeField] private Button _button;
+        [SerializeField] private RectTransform _rectTransform;
+        public Action<Vector3> OnButtonClicked;
+        private Camera _camera;
+        private Cover _cover;
     
-    private void OnDisable()
-    {
-        _button.onClick.RemoveListener(OnButtonClickedAdd);
-    }
-
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
     
+        private void OnEnable()
+        {
+            _button.onClick.AddListener(OnButtonClickedAdd);
+        }
+    
+        public void SetPosition(Cover cover, RectTransform parentRect)
+        {
+            _cover = cover;
+            var yOffset = _cover.transform.localScale.y;
+            var worldToScreenPoint = _camera.WorldToScreenPoint(cover.transform.position + Vector3.up * yOffset);
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                parentRect, worldToScreenPoint, null, out var localPoint);
+            _rectTransform.anchoredPosition = localPoint;
+        }
+    
+        private void OnButtonClickedAdd()
+        {
+            gameObject.SetActive(false);
+            OnButtonClicked?.Invoke(_cover.GetProperPosition());
+        }
+        
+        private void OnDisable()
+        {
+            _button.onClick.RemoveListener(OnButtonClickedAdd);
+        }
+    }
 }
